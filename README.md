037ece0461d5d4655689c8769e70c150a051f7a0
Fabian Meyer <user8324@posteo.net>
2018-03-05 15:39:40 +0100

    Update package.sh

---
+++ README.md
@@ -0,0 +1,17 @@
+# Atom Settings
+
+This repo contains my settings for the Atom texteditor.
+
+Clone into ```~/.atom```.
+
+Install Atom packages:
+
+```
+./package.sh install
+```
+
+Create backup of Atom packages currently installed:
+
+```
+./package.sh backup
+```
--- package.sh
+++ package.sh
@@ -1,3 +1,27 @@
 #!/bin/bash -e
 
-apm install --packages-file ~/.atom/package.list
+usage()
+{
+    echo "usage: ./package.sh install|backup"
+}
+
+if [ $# != 1 ]
+then
+    echo "Error. Wrong amount of arguments."
+    usage
+    exit 1
+fi
+
+case "$1" in
+    "install")
+        apm install --packages-file package.list
+        ;;
+    "backup")
+        apm list --installed --bare > package.list
+        ;;
+    *)
+        echo "Error. Invalid argument $1"
+        usage
+        exit 1
+        ;;
+esac
